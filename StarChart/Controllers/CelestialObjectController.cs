bb91a59cde55fd6138c91d2c6e25909f835cc541
codevop <bretknoll@gmail.com>
2021-08-11 13:55:58 -0600

    Completed module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -12,11 +13,18 @@ namespace StarChart.Controllers
     [Route("")]
     public class CelestialObjectController : ControllerBase
     {
-        private readonly ApplicationDbContext _context; 
+        private readonly ApplicationDbContext _context;
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
         }
 
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id) {
+        }
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name) { }
+
+        public IActionResult GetAll() { }
     }
 }
